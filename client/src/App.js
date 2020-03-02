b1f57be34fd7ee00dfbf866d5df79a183e0e3e5c
sdavidson90 <s.davidson90@hotmail.ca>
2020-03-02 14:53:07 -0800

    start

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/main.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
