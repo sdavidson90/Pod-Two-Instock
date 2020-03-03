e1201bd160469097c64915520ba61b5105881dfe
jchan96 <jchan96@hotmail.com>
2020-03-03 09:47:10 -0800

    basics for header done

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./styles/main.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./component/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
-}
+const App = () => {
+  // return <h1>hi</h1>;
+  return <Header />;
+};
 
 export default App;
