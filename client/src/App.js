c9325cdca4056399175d12cd33955ec367e5c193
Marian <24884345+aemarian@users.noreply.github.com>
2020-03-03 15:19:36 -0800

    POST singlepaths inve components uploaded

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,16 @@ import {
 } from "react-router-dom";
 import "./styles/main.css";
 import Product from "./components/Product";
+import Inventory from "./components/Inventory";
 
 function App() {
-
   return (
     <Router>
       <Product />
+      {/* <Inventory /> */}
       <Switch>
         {/* <Route path="/locations" component={} /> */}
-        {/* <Route path="/inventory" component={} /> */}
+        <Route path="/inventory" component={Inventory} />
         <Route path="/product/:id" component={Product} />
         {/* <Route path="/" component={} exact /> */}
       </Switch>
--- server/routes/api/product.js
+++ server/routes/api/product.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const uuid = require("uuid/v4");
+const fs = require("fs");
 
 const productsFile = __dirname + "/../../models/product.json";
 const products = require(productsFile);
@@ -32,6 +33,32 @@ router.get("/:id", (req, res) => {
     res.json(selectedProduct);
     console.log(selectedProduct);
   }
+});
+
+function writeJSONFile(filename, content) {
+  console.log(filename, content);
+  fs.writeFileSync(filename, JSON.stringify(content), "utf8", err => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  console.log(`changes saved to file ${filename}....`);
+}
+
+router.post("/", (req, res) => {
+  const newProduct = {
+    id: uuid(),
+    description: req.body.description,
+    ordered: req.body.ordered,
+    reference: req.body.reference,
+    last: req.body.last,
+    location: req.body.location,
+    quantity: req.body.quantity,
+    categories: req.body.categories
+  };
+  products.push(newProduct);
+  writeJSONFile(productsFile, products);
+  res.json(products);
 });
 
 module.exports = router;
